dc6b85b6265e5e6dd7ef3f6c8b6a378f42d82355
Meriel Christialou Boadilla <143819258+merielboadilla@users.noreply.github.com>
2024-08-31 08:48:04 +0800

    Add files via upload

---
+++ grp2_boadilla_ex1.py
@@ -0,0 +1,33 @@
+# My first simple script
+print("Hello, I'm Meriel Christialou M. Boadilla! BSCS 3F")
+
+# Variables
+num = 21
+name = "Meriel"
+
+print("Number:", num)
+print("Name:", name)
+
+# Variable Names
+mohanida = "Palao"
+cecille = "Aringo"
+catherine = "Rodriguez"
+jonalyn = "Frolles"
+
+print("Group Members:")
+print("Mohanida:", mohanida)
+print("Cecille:", cecille)
+print("Catherine:", catherine)
+print("Jonalyn:", jonalyn)
+
+# Many Values to Multiple Variables
+x, y, z = 10, 20, 30
+print("X:", x)
+print("Y:", y)
+print("Z:", z)
+
+# One Value to Multiple Variables
+a = b = c = 50
+print("A:", a)
+print("B:", b)
+print("C:", c)
