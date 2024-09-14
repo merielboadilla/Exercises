0c0c3e297ce1fc0670930f90751639836a01159e
Meriel Christialou Boadilla <143819258+merielboadilla@users.noreply.github.com>
2024-09-14 09:55:07 +0800

    Add files via upload

---
+++ grp2_boadilla_ex4.py
@@ -0,0 +1,22 @@
+# If statement with condition returning True or False
+num = 10
+if num > 5:
+    print("Condition is True")  # Returns True
+else:
+    print("Condition is False")  # Returns False
+
+# List example with mixed data types
+my_list = ["strawberry", 10, True]
+print(my_list)
+
+# Tuple example with mixed data types
+my_tuple = ("grapes", 20, False)
+print(my_tuple)
+
+# Set example with mixed data types
+my_set = {"orange", 30, True}
+print(my_set)
+
+# Dictionary example with mixed data types
+my_dict = {"name": "meriel", "age": 21, "is_student": True}
+print(my_dict)
